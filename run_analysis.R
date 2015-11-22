bfc0f787df6a8ba234d2eb521894b0b305edb70e
Victor R Zambrano <donvito4571@gmail.com>
2015-11-22 00:16:55 -0430

    Incluir Sujeto.Actividad

--- run_analysis.R
+++ run_analysis.R
@@ -9,14 +9,13 @@ run_analysis <- function(){
     Ytest <- fread("test/y_test.txt")
     Ytotal <- c(activities[Ytrain$V1]$V2, activities[Ytest$V1]$V2)
     columnNames <- titles[cols]$V2
-    #columnNames <- c(columnNames, "Activity")
-    #reduced_train <- cbind(reduced_Xtrain, activities[Ytrain$V1]$V2)
-    #reduced_test <- cbind(reduced_Xtest, activities[Ytest$V1]$V2)
     total_data <- rbind(reduced_Xtrain, reduced_Xtest)
     colnames(total_data) <- columnNames
     train_subjects <- fread("train/subject_train.txt")
     test_subjects <- fread("test/subject_test.txt")
     subjects <- c(train_subjects$V1, test_subjects$V1)
     tidy_data <- sapply(split(total_data, list(subjects, Ytotal)), colMeans)
+    tidy_data <- rbind(colnames(tidy_data), tidy_data)
+    rownames(tidy_data)[1] <- "Subject.Activity"
     write.table(t(tidy_data), file = "tidy_data_set.txt", row.names = FALSE)
 }
